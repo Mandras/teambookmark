b5a4cd451d0c99eb1ff27e975dbb6db3000dabe8
Romain <rbenoit@digitaddict.com>
2019-11-04 14:44:06 +0700

    cross browser rework

--- js/team-bookmark.js
+++ js/team-bookmark.js
@@ -1,41 +1,133 @@
 var domain = "https://www.teambookmark.org";
+var folder_name_pre = "✔️ ";
+var is_chrome = false;
 var root_id = '';
 var main_id = '';
 var folder_name;
 var version = 0;
 var options;
 
-// DO PING
+function ff_error(error) { console.log(`An error: ${error}`); }
 
-function ping() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', domain + '/ajax/ping.php?key=' + options.key + '&version=' + version + '&ts=' + new Date().getTime(), true);
-	xhr.responseType = 'json';
-
-	xhr.onload = function() {
-		if (xhr.status === 200) {
-
-			var obj = xhr.response;
-
-			if (obj.version > 0 && obj.version > version) {
-				synchronize();
+// sub-function => synchronize_folder_found
+function synchronize_folder_found(items) {
+	if (items.length > 1) {
+		for (var i = 0; i < items.length; i++) {
+			if (is_chrome) {
+				browser.bookmarks.removeTree(items[i].id);
+			}
+			else {
+				var removing = browser.bookmarks.removeTree(items[i].id);
+				removing.then(function() { }, ff_error);
 			}
 		}
-	};
-	xhr.send();
+	}
+
+	if (items.length != 1 && root_id.length > 0) {
+		if (is_chrome) {
+			browser.bookmarks.create({
+				parentId: root_id,
+				title: folder_name_pre + folder_name,
+				index: 0
+			}, function(node) {
+				console.info("team-bookmark: create root for toolbar");
+				main_id = node.id;
+				compare_bookmark(node, this.bookmark);
+			});
+		}
+		else {
+			var creation = browser.bookmarks.create({
+				parentId: root_id,
+				title: folder_name_pre + folder_name,
+				index: 0
+			});
+			creation.then(function(node) {
+				console.info("team-bookmark: create root for toolbar");
+				main_id = node.id;
+				compare_bookmark(node, this.bookmark);
+			});
+		}		
+	}
+	else if (items.length == 1) {
+		main_id = items[0].id;
+		compare_bookmark(items[0], this.bookmark);
+	}
+
+	setTimeout(function() {
+		browser.bookmarks.onCreated.addListener(update_on_cloud);
+		browser.bookmarks.onRemoved.addListener(update_on_cloud);
+		browser.bookmarks.onChanged.addListener(update_on_cloud);
+		browser.bookmarks.onMoved.addListener(update_on_cloud);
+	}, 1500);
 }
 
-// TRIGGER A CHANGE, UPDATE CLOUD DATAS
+// sub-function => synchronize_after_storage
+function synchronize_after_storage(item) {
+	if (typeof item.options !== "undefined" && typeof item.options.key !== "undefined") {
+		options = {
+			key: item.options.key
+		};
 
-function update_on_cloud() {
-	var get = browser.bookmarks.getSubTree(main_id);
-	get.then(function(node) {
+		if (options.key.length > 0) {
 
-		if (node[0].title != folder_name) {
-			var updating = browser.bookmarks.update(node[0].id, {
-				title: folder_name
-			});
-			updating.then(function() { }, function() { });
+			console.info("team-bookmark: launch synchronize");
+
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', domain + '/ajax/synchronize.php?key=' + options.key + '&ts=' + new Date().getTime(), true);
+			xhr.responseType = 'json';
+
+			xhr.onload = function() {
+				if (xhr.status === 200) {
+
+					var obj = xhr.response;
+
+					if (obj.app.name == "team-bookmark" && typeof obj.folder_name != "undefined" && obj.folder_name.length > 0) {
+
+						folder_name = obj.folder_name;
+
+						version = obj.meta.version;
+
+						if (browser.bookmarks.onCreated.hasListener(update_on_cloud)) { browser.bookmarks.onCreated.removeListener(update_on_cloud); }
+						if (browser.bookmarks.onRemoved.hasListener(update_on_cloud)) { browser.bookmarks.onRemoved.removeListener(update_on_cloud); }
+						if (browser.bookmarks.onChanged.hasListener(update_on_cloud)) { browser.bookmarks.onChanged.removeListener(update_on_cloud); }
+						if (browser.bookmarks.onMoved.hasListener(update_on_cloud))   { browser.bookmarks.onMoved.removeListener(update_on_cloud);   }
+
+						if (is_chrome) {
+							browser.bookmarks.search({ title: folder_name_pre + folder_name }, synchronize_folder_found.bind({bookmark: obj.bookmarks}));
+						}
+						else {
+							var searching = browser.bookmarks.search({ title: folder_name_pre + folder_name });
+							searching.then(synchronize_folder_found.bind({bookmark: obj.bookmarks}), ff_error);
+						}
+					}
+				}
+				else {
+					console.error("team-bookmark: could not synchronize, XHR responses KO");
+				}
+			};
+			xhr.send();
+		}
+		else { console.info("team-bookmark: no key defined."); }
+	}
+	else { console.info("team-bookmark: options not yet defined."); }
+}
+
+// sub-function => update_continue_process
+function update_continue_process(node) {
+	if (typeof node != "undefined") {
+		if (node[0].title != folder_name_pre + folder_name) {
+			if (is_chrome) {
+				browser.bookmarks.update(node[0].id, {
+					title: folder_name_pre + folder_name
+				});
+			}
+			else {
+				var updating = browser.bookmarks.update(node[0].id, {
+					title: folder_name_pre + folder_name
+				});
+				updating.then(function() { }, ff_error);
+			}
+			
 		}
 
 		var json = JSON.stringify(node[0].children);
@@ -43,6 +135,7 @@ function update_on_cloud() {
 		var data = new FormData();
 		data.append('key', options.key);
 		data.append('version', version);
+		data.append('is_chrome', is_chrome);
 		data.append('json', json);
 
 		var xhr = new XMLHttpRequest();
@@ -53,228 +146,207 @@ function update_on_cloud() {
 
 				var obj = xhr.response;
 
-				console.log(obj);
-
 				if (obj.action == "update_version" && obj.version > 0) {
 					version = obj.version;
 				}
 			}
 		};
 		xhr.send(data);
+	}
+	else { version = 0; }
+}
 
-	}, function() { console.error("team-bookmark - can not find the team folder, have you delete it ? ... how dare you ?"); });
+// sub-function => compare_subtree
+function compare_subtree(node) {
+	for (var i = 0; i < node[0].children.length; i++) {
+		if ((is_chrome && typeof node[0].children[i].url == "undefined") || (!is_chrome && node[0].children[i].type == "folder")) {
+			if (is_chrome) {
+				browser.bookmarks.removeTree(node[0].children[i].id);
+			}
+			else {
+				var removing = browser.bookmarks.removeTree(node[0].children[i].id);
+				removing.then(function() { }, ff_error);
+			}			
+		}
+		else {
+			if (is_chrome) {
+				browser.bookmarks.remove(node[0].children[i].id);
+			}
+			else {
+				var removing = browser.bookmarks.remove(node[0].children[i].id);
+				removing.then(function() { }, ff_error);
+			}
+		}
+	}
+
+	var system_tree = this.system_tree;
+	var tb_tree = this.tb_tree;
+
+	setTimeout(function() {
+
+		var system_tree = this.system_tree;
+		var tb_tree = this.tb_tree;
+
+		var index = 0;
+		if (typeof tb_tree != "undefined") {
+			for (var i = 0; i < tb_tree.length; i++) {
+				/* BOOKMARK */
+				if (tb_tree[i].url != null) {
+					if (is_chrome) {
+						browser.bookmarks.create({
+							parentId: system_tree.id,
+							title: tb_tree[i].title,
+							url: tb_tree[i].url,
+							index: tb_tree[i].index
+						});
+					}
+					else {
+						var creation = browser.bookmarks.create({
+							parentId: system_tree.id,
+							title: tb_tree[i].title,
+							url: tb_tree[i].url,
+							index: tb_tree[i].index
+						});
+						creation.then(function(node) { });
+					}
+
+					index++;
+				}
+				/* FOLDER */
+				else if (tb_tree[i].title != null && tb_tree[i].title.length > 0) {
+					if (is_chrome) {
+						browser.bookmarks.create({
+							parentId: system_tree.id,
+							title: tb_tree[i].title,
+							index: tb_tree[i].index
+						}, function(node) {
+							compare_bookmark(node, this.newtree);
+						}.bind({newtree: tb_tree[i].bookmarks}));
+					}
+					else {
+						var creation = browser.bookmarks.create({
+							parentId: system_tree.id,
+							title: tb_tree[i].title,
+							index: tb_tree[i].index
+						});
+						creation.then(function(node) {
+							compare_bookmark(node, this.newtree);
+						}.bind({newtree: tb_tree[i].bookmarks}));
+					}
+
+					index++;
+				}
+				/* SEPARATOR */
+				else if (!is_chrome) {
+					var creation = browser.bookmarks.create({
+						parentId: system_tree.id,
+						type: "separator",
+						title: '',
+						url: '',
+						index: tb_tree[i].index
+					});
+					creation.then(function(node) { });
+
+					index++;
+				}
+			}
+		}
+		else {
+			console.error("empty tb_tree, reseting local version");
+			version = 0;
+		}
+	}.bind({tb_tree: tb_tree, system_tree: system_tree}), 50);
+}
+
+// DO PING
+
+function ping() {
+	if (typeof options != "undefined" && typeof options.key != "undefined") {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', domain + '/ajax/ping.php?key=' + options.key + '&version=' + version + '&ts=' + new Date().getTime(), true);
+		xhr.responseType = 'json';
+
+		xhr.onload = function() {
+			if (xhr.status === 200) {
+
+				var obj = xhr.response;
+
+				if (obj.version > 0 && obj.version > version) {
+					synchronize();
+				}
+			}
+		};
+		xhr.send();
+	}
+	else {
+		synchronize();
+	}
+}
+
+// TRIGGER A CHANGE, UPDATE CLOUD DATAS
+
+function update_on_cloud() {
+	if (is_chrome) {
+		browser.bookmarks.getSubTree(main_id, update_continue_process);
+	}
+	else {
+		var get = browser.bookmarks.getSubTree(main_id);
+		get.then(update_continue_process, ff_error);
+	}
 }
 
 // WE DO THE CHANGES IN BOOKMARKS
 
 function compare_bookmark(system_tree, tb_tree) {
-
 	if (main_id.length > 0) {
-
-		var get = browser.bookmarks.getSubTree(system_tree.id);
-		get.then(function(node) {
-			for (var i = 0; i < node[0].children.length; i++) {
-				if (node[0].children[i].type == "folder") {
-					var removing = browser.bookmarks.removeTree(node[0].children[i].id);
-	    			removing.then(function() { }, function() { });
-				}
-				else {
-					var removing = browser.bookmarks.remove(node[0].children[i].id);
-					removing.then(function() { });
-				}
-			}
-
-			setTimeout(function() {
-				for (var i = 0; i < tb_tree.length; i++) {
-					// -- BOOKMARK
-					if (tb_tree[i].url != null) {
-						var creation = browser.bookmarks.create({
-							parentId: system_tree.id,
-							title: tb_tree[i].title,
-							url: tb_tree[i].url,
-							index: i
-						});
-						creation.then(function(node) { });
-					}
-					// -- FOLDER
-					else if (tb_tree[i].title != null && tb_tree[i].title.length > 0) {
-						var creation = browser.bookmarks.create({
-							parentId: system_tree.id,
-							title: tb_tree[i].title,
-							index: i
-						});
-						var tb_newtree = tb_tree[i].bookmarks;
-						creation.then(function(node) {
-							compare_bookmark(node, tb_newtree);
-						});
-					}
-					// -- SEPARATOR
-					else {
-						var creation = browser.bookmarks.create({
-							parentId: system_tree.id,
-							type: "separator",
-							title: '',
-							url: '',
-							index: i
-						});
-						creation.then(function(node) { });
-					}
-				}
-			}, 50);
-		}, function() { });
+		if (is_chrome) {
+			browser.bookmarks.getSubTree(system_tree.id, compare_subtree.bind({tb_tree: tb_tree, system_tree: system_tree}));
+		}
+		else {
+			var get = browser.bookmarks.getSubTree(system_tree.id);
+			get.then(compare_subtree.bind({tb_tree: tb_tree, system_tree: system_tree}), ff_error);
+		}
 	}
 }
 
 // FULL SYNCHRONIZE
 
 function synchronize() {
-
-	// get options
-
-	var storage = browser.storage.local.get("options");
-
-	storage.then(function(item) {
-		if (typeof item.options !== "undefined" && typeof item.options.key !== "undefined") {
-			options = {
-				key: item.options.key
-			};
-
-			if (options.key.length > 0) {
-
-				console.info("team-bookmark: launch synchronize");
-
-				// Get JSON content
-
-				var xhr = new XMLHttpRequest();
-				xhr.open('GET', domain + '/ajax/synchronize.php?key=' + options.key + '&ts=' + new Date().getTime(), true);
-				xhr.responseType = 'json';
-
-				xhr.onload = function() {
-					if (xhr.status === 200) {
-
-						var obj = xhr.response;
-
-						if (obj.app.name == "team-bookmark" && typeof obj.folder_name != "undefined" && obj.folder_name.length > 0) {
-
-							folder_name = obj.folder_name;
-
-							version = obj.meta.version;
-
-							// Because we override all bookmarks, we remove binds
-
-							if (browser.bookmarks.onCreated.hasListener(update_on_cloud)) { browser.bookmarks.onCreated.removeListener(update_on_cloud); }
-							if (browser.bookmarks.onRemoved.hasListener(update_on_cloud)) { browser.bookmarks.onRemoved.removeListener(update_on_cloud); }
-							if (browser.bookmarks.onChanged.hasListener(update_on_cloud)) { browser.bookmarks.onChanged.removeListener(update_on_cloud); }
-							if (browser.bookmarks.onMoved.hasListener(update_on_cloud)) { browser.bookmarks.onMoved.removeListener(update_on_cloud); }
-
-							// Let's search for the folder
-
-							var searching = browser.bookmarks.search({ title: folder_name });
-							searching.then(function(items) {
-								if (items.length > 1) {
-									for (var i = 0; i < items.length; i++) {
-										var removing = browser.bookmarks.removeTree(items[i].id);
-			    						removing.then(function() { }, function() { });
-									}
-								}
-								
-								if (items.length != 1 && root_id.length > 0) {
-									var creation = browser.bookmarks.create({
-										parentId: root_id,
-										title: folder_name,
-										index: 0
-									});
-									creation.then(function(node) {
-										console.info("team-bookmark: create root for toolbar");
-										main_id = node.id;
-										compare_bookmark(node, obj.bookmarks);
-									});
-								}
-								else if (items.length == 1) {
-									main_id = items[0].id;
-									compare_bookmark(items[0], obj.bookmarks);
-								}
-
-								setTimeout(function() {
-									browser.bookmarks.onCreated.addListener(update_on_cloud);
-									browser.bookmarks.onRemoved.addListener(update_on_cloud);
-									browser.bookmarks.onChanged.addListener(update_on_cloud);
-									browser.bookmarks.onMoved.addListener(update_on_cloud);
-								}, 1500);
-
-							}, function() { });
-						}
-					}
-					else {
-						console.error("team-bookmark: could not synchronize, XHR responses KO");
-					}
-				};
-				xhr.send();
-			}
-			else { console.error("team-bookmark: no key defined."); }
-		}
-		else { console.error("team-bookmark: options missmatch datas."); }
-	},
-	function() {
-		console.error("team-bookmark: no options found.");
-	});
+	if (is_chrome) {
+		browser.storage.local.get("options", synchronize_after_storage);
+	}
+	else {
+		var storage = browser.storage.local.get("options");
+		storage.then(synchronize_after_storage, ff_error);
+	}
 }
-
-console.info("team-bookmark: starting");
 
 // STARTUP
 
+console.info("team-bookmark: starting");
+
+var is_chrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (is_chrome) { var browser = chrome; }
+
 // search for default root_id
 
-var tree = browser.bookmarks.getTree();
-tree.then(function(items) {
-	for (var i = 0; i < items[0].children.length; i++) {
-		if (items[0].children[i].id.startsWith('toolbar_')) {
-			root_id = items[0].children[i].id;
-		}
-	}
-});
-
-// we set the new menus for FF only (unused)
-
-if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-	browser.menus.create({
-		id: "tb-main",
-		type: "checkbox",
-		title: browser.i18n.getMessage("menuUseThisFolder"),
-		contexts: ["bookmark"],
-		enabled: false,
-		checked: false
-	}, function() { });
-
-	browser.menus.onShown.addListener(async function(info, tab) {
-		if (typeof info.bookmarkId != "undefined" && info.bookmarkId.length > 0) {
-			var get = browser.bookmarks.getSubTree(info.bookmarkId);
-			get.then(function(node) {
-
-				if (main_id.length > 0 && node[0].id == main_id) {
-					var checked = true;
-				}
-				else { var checked = false; }
-
-				if (main_id.length == 0 && node[0].type == "folder" && node[0].children.length == 0) {
-					var updating = browser.menus.update("tb-main", {
-						checked: checked,
-						enabled: false
-					});
-					updating.then(function() { browser.menus.refresh(); }, function() { });
-				}
-				else {
-					var updating = browser.menus.update("tb-main", {
-						checked: checked,
-						enabled: false
-					});
-					updating.then(function() { browser.menus.refresh(); }, function() { });
-				}
-			});
-		}
+if (is_chrome) {
+	browser.bookmarks.getTree(function(items) {
+		root_id = items[0].children[0].id;
 	});
 }
+else {
+	var tree = browser.bookmarks.getTree();
+	tree.then(function(items) {
+		for (var i = 0; i < items[0].children.length; i++) {
+			if (items[0].children[i].id.startsWith('toolbar_')) {
+				root_id = items[0].children[i].id;
+			}
+		}
+		if (root_id.length == 0) { root_id = items[0].children[0].id; }
+	}, ff_error);
+}
+
 
 // launch auto synch
 
--- options/script.js
+++ options/script.js
@@ -4,26 +4,48 @@
 "use strict";
 
 const $ = document.getElementById.bind(document);
-const logError = console.exception.bind(console);
+
+function ff_error(error) { console.log(`An error: ${error}`); }
+
+function after_storage_set() {
+	$("saved").classList.add("saving");
+	setTimeout(function() { $("saved").classList.remove("saving"); }, 2000);
+}
+
+function after_storage_get(item) {
+	if (typeof item.options.key !== "undefined") {
+		$("key").value = item.options.key.trim()
+	}
+}
+
+var is_chrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (is_chrome) { var browser = chrome; }
 
 const save_options = () => {
-	browser.storage.local.set({
-		options:
-			{
+
+	if (is_chrome) {
+		browser.storage.local.set({
+			options: {
 				key: $("key").value.trim()
 			}
-	}).then(function() {
-		$("saved").classList.add("saving");
-		setTimeout(function() { $("saved").classList.remove("saving"); }, 2000);
-	}, logError);
+		}, after_storage_set);
+	}
+	else {
+		browser.storage.local.set({
+			options: {
+				key: $("key").value.trim()
+			}
+		}).then(after_storage_set, ff_error);
+	}	
 };
 
 const restore_options = () => {
-	browser.storage.local.get("options").then(function(item) {
-		if (typeof item.options.key !== "undefined") {
-			$("key").value = item.options.key.trim()
-		}
-	}, logError);
+	if (is_chrome) {
+		browser.storage.local.get("options", after_storage_get);
+	}
+	else {
+		browser.storage.local.get("options").then(after_storage_get, ff_error);
+	}
 };
 
 document.addEventListener("DOMContentLoaded", restore_options);
